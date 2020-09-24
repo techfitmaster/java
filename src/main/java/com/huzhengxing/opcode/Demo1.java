d7eb2267a8ae0bd8299692bd0d2fd8c500ebea32
Albert <1092144169@qq.com>
2020-09-24 16:12:32 +0800

    修改项目结构

---
+++ src/main/java/com/huzhengxing/opcode/Demo1.java
@@ -0,0 +1,12 @@
+package com.huzhengxing.opcode;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int sum = i * j;
+        String str = "1";
+        System.out.println(sum + str);
+    }
+}
